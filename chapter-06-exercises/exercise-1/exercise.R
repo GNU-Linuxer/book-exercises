5bd4a5bb03fab3d820400aa4359ba5e36e552fb1
David Xie <xdkdavid@gmail.com>
2020-01-17 21:01:39 -0800

    complete chapter 6 exercise 1

--- chapter-06-exercises/exercise-1/exercise.R
+++ chapter-06-exercises/exercise-1/exercise.R
@@ -21,26 +21,29 @@ now_doing <- toupper(now_doing)
 
 # Pick two of your favorite numbers (between 1 and 100) and assign them to 
 # variables `fav_1` and `fav_2`
-
+fav_1 <- 50
+fav_2 <- 75
 
 # Divide each number by the square root of 201 and save the new value in the
 # original variable
-
+fav_1 <- fav_1/(sqrt(201))
+fav_2 <- fav_2/(sqrt(201))
 
 # Create a variable `raw_sum` that is the sum of the two variables. Use the 
 # `sum()` function for practice.
-
+raw_sum <- sum(fav_1,fav_2)
 
 # Create a variable `round_sum` that is the `raw_sum` rounded to 1 decimal place.
 # Use the `round()` function.
-
+round_sum <- round(raw_sum,1)
 
 # Create two new variables `round_1` and `round_2` that are your `fav_1` and 
 # `fav_2` variables rounded to 1 decimal places
-
+round_1 <- round(fav_1,1)
+round_2 <- round(fav_2,1)
 
 # Create a variable `sum_round` that is the sum of the rounded values
-
+sum_round <- round_1+round_2
 
 # Which is bigger, `round_sum` or `sum_round`? (You can use the `max()` function!)
-
+# both are the same
