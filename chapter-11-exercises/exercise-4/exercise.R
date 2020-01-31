64b4c07588bc5d8f27c281d99b61a27490cb0ea2
David Xie <xdkdavid@gmail.com>
2020-01-31 11:47:24 -0800

    finish chapter 11 exercise 4

--- chapter-11-exercises/exercise-4/exercise.R
+++ chapter-11-exercises/exercise-4/exercise.R
@@ -2,7 +2,8 @@
 
 # Install the `"nycflights13"` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
-
+library(dplyr)
+library(nycflights13)
 
 # The data frame `flights` should now be accessible to you.
 # Use functions to inspect it: how many rows and columns does it have?
@@ -14,11 +15,11 @@
 # Use `dplyr` to give the data frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
-
+my_flights <- mutate(flights, delay_in_flight = (arr_delay - dep_delay))
 
 # Use `dplyr` to sort your data frame in descending order by the column you just
 # created. Remember to save this as a variable (or in the same one!)
-
+my_flights <- arrange(my_flights, delay_in_flight)
 
 # For practice, repeat the last 2 steps in a single statement using the pipe
 # operator. You can clear your environmental variables to "reset" the data frame
@@ -27,16 +28,21 @@
 # Make a histogram of the amount of time gained using the `hist()` function
 
 
+
 # On average, did flights gain or lose time?
 # Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
-
+# A summarizing operation
+average_lose_time <- summarise(my_flights, mean_delay_in_flight = mean(delay_in_flight, na.rm = TRUE))
+print(average_lose_time)
 
 # Create a data.frame of flights headed to SeaTac ('SEA'), only including the
 # origin, destination, and the "gain_in_air" column you just created
-
+to_SEA <- filter(my_flights, dest == "SEA")
+to_SEA <- select(to_SEA, origin, dest, delay_in_flight)
 
 # On average, did flights to SeaTac gain or loose time?
-
+average_lose_time_to_SEA <- summarise(to_SEA, mean_delay_in_flight = mean(delay_in_flight, na.rm = TRUE))
+print(average_lose_time_to_SEA)
 
 # Consider flights from JFK to SEA. What was the average, min, and max air time
 # of those flights? Bonus: use pipes to answer this question in one statement
